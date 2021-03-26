0786026c099336be482de68805e89bbdd41b1ba8
Yauhen Ashkinadze <r0var@outlook.com>
2021-03-26 19:42:14 +0300

    script updated

--- HW10/printff.sh
+++ HW10/printff.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-function findFiles {
-  find -type f \( -name "*.png" -o -name "*.sh" \)
-}
+FOLD=$2
+FTYPE1="*.png"
+FTYPE2="*.sh"
 
-function findAll {
-  find
+function findFiles {
+  find $FOLD -type f \( -name "$FTYPE1" -o -name "$FTYPE2" \)
 }
 
 function printHelp {
-  echo -e "   --help        Shows help \n \
-  --findf       Find .sh and .png files in subdirectories \n \
-  --find        Find all files and folders in subdiectories"
+  echo -e "   --help                  Shows help \n \
+  --findf [PATH]          Find .sh and .png files in subdirectories \n \
+  --find [PATH]           Find all files and folders in subdiectories"
 }
 
 case $1 in
     --findf) findFiles;;
-    --find) findAll;;
+    --find) find $FOLD;;
     --help) printHelp;;
     *) echo "Try './printff.sh --help' for more information.";;
 esac
